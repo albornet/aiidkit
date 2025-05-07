1b300f1d772c29b21568bbfb664504d665220d66
albornet <alban.bornet@gmail.com>
2025-05-07 16:04:50 +0200

    added new feature names

--- process_data.py
+++ process_data.py
@@ -1,42 +1,38 @@
 import os
+import re
 import pandas as pd
+from tqdm import tqdm
 from process_data_utils import DataConfig as cfg
 
 
-############
-# COMMENTS #
-############
-
-# BL:  BASELINE
-# FUP: FOLLOW-UP
-# ID:  MAY SOMETIMES MEAN INFECTION DISEASE (BUT USUALLY IDENTIFIER...)
-# PAT: PATIENT
-
-# - #1_CONSENT:    skipped (only consent info from this one, abandonned for #9_ORGAN_BASE)
-# - #2_KIDNEY_BL:  not done (contains transplantation + donor info)
-# - #3_KIDNEY_FUP: not done
-# - #4_PAT_BL:     not done
-# - #5_PAT_PSQ:    done (only ethnicity info)
-# - #6_PAT_ID:     not done (may be from where target labels can be extracted)
-# - #7_PAT_DRUG:   not done (pay attention: some drugs could leak label information)
-# - #8_PAT_STOP:   not done (may be from where censoring info can be extracted)
-# - #9_ORGAN_BASE: not done (contains a lot of static info and also about censoring)
-
-############
-# COMMENTS #
-############
-
-
 def main() -> None:
-    """ ...
-    """
     raw_data_dict: dict[str, pd.DataFrame]
-    raw_data_dict = pd.read_excel(cfg.RAW_DATA_PATH, sheet_name=None)
-    process_data(data_dict=raw_data_dict)
+    raw_data_dict = pd.read_pickle(cfg.RAW_DATA_PATH_PICKLE)
+    # raw_data_dict = pd.read_excel(cfg.RAW_DATA_PATH, sheet_name=None)
+    raw_data_dict = {re.sub(r'#\d_', '', k).lower(): v for k, v in raw_data_dict.items()}
+    if not cfg.DEBUG:
+        create_eavt_patient_records(data_dict=raw_data_dict)
+    else:
+        explore_data(raw_data_dict)
 
 
-def process_data(data_dict: dict[str, pd.DataFrame]) -> None:
-    """ ...
+def explore_data(data_dict: dict[str, pd.DataFrame]) -> None:
+    """ Explore the data for debugging, without creating patient records
+    """
+    consent = data_dict["consent"]
+    kidney_bl = data_dict["kidney_bl"]
+    kidney_fup = data_dict["kidney_fup"]
+    pat_bl= data_dict["pat_bl"]
+    pat_psq = data_dict["pat_psq"]
+    pat_id = data_dict["pat_id"]
+    pat_drug = data_dict["pat_drug"]
+    pat_stop = data_dict["pat_stop"]
+    organ_base = data_dict["organ_base"]
+    import ipdb; ipdb.set_trace()
+
+
+def create_eavt_patient_records(data_dict: dict[str, pd.DataFrame]) -> None:
+    """ Create patient records in EAVT format (entity, attribute, value, time)
     """
     # Initialize patients (and only keep the ones with consent)
     patient_ids_with_consent = get_pat_ids_with_consent(data_dict)
@@ -46,12 +42,21 @@ def process_data(data_dict: dict[str, pd.DataFrame]) -> None:
     }
     
     # Iterate over all sheets of the excel file data to fill patient records
+    all_feats = pd.DataFrame()
     for sheet_name, sheet_df in data_dict.items():
         if sheet_name in cfg.TAKEN_DF_NAMES:
-            print(f"Processing {sheet_name} sheet")
+            print(f"Processing sheet {sheet_name}")
             data_df = base_filtering(sheet_df, patient_ids_with_consent)
-            add_static_features_to_patient_records(patient_records, data_df, sheet_name)
-            add_longitudinal_features_to_patient_records(patient_records, data_df, sheet_name)
+            
+            # Extract static and longitudinal features from sheet
+            stat_feats = extract_static_features(data_df, sheet_name)
+            long_feats = extract_longitudinal_features(data_df, sheet_name)
+            all_feats = pd.concat([all_feats, stat_feats, long_feats], ignore_index=True)
+            
+    # Populate the patient records with identified features, sorted by timestamp
+    all_feats["time"] = pd.to_datetime(all_feats["time"], errors="coerce")
+    all_feats = all_feats.sort_values(by="time")
+    add_features_to_patient_records(patient_records, all_feats.drop_duplicates())
     
     # Save each patient record to a csv file
     os.makedirs(cfg.SAVE_DIR, exist_ok=True)
@@ -60,46 +65,52 @@ def process_data(data_dict: dict[str, pd.DataFrame]) -> None:
         pat_df.to_csv(save_path, index=False)
 
 
-def add_static_features_to_patient_records(
-    patient_records: dict[int, pd.DataFrame],
+def extract_static_features(
     data_df: pd.DataFrame,
     data_key: str,
 ) -> None:
-    """ Add static features to patient records (...)
+    """ Add static features to patient records
     """
     # Extract static features data
+    print(f" - Extracting static features")
     feat_dict: dict = cfg.STATIC_FEAT_INFO_DICTS[data_key]
     static_df = data_df[["patid"] + list(feat_dict.keys())]
-    static_df = static_df.melt(  # tall format: columns -> attribute/value rows
+    static_df = static_df.melt(  # tall format: columns -> attribute / value rows
         id_vars=["patid"],
         value_vars=feat_dict.keys(),
         var_name="attribute",
         value_name="value",
     )
+    static_df["entity"] = data_key
+    static_df["time"] = pd.NaT  # no time for static features
     
-    # Populate patient records with static features
-    static_df["entity"] = data_key  # could depend on a mapping between keys and data_key, e.g., one type of test in PAT_BL?
-    static_df["time"] = pd.NaT  # static features are assigned time "not a time"
-    for pat_id, group in static_df.groupby("patid", as_index=False):
-        patient_records[pat_id] = pd.concat([patient_records[pat_id], group], ignore_index=True)
+    # Invert value and time columns for "static" features that are dates
+    time_attrs = [attr for attr, dtype in feat_dict.items() if dtype == pd.Timestamp]
+    is_a_time = static_df["attribute"].isin(time_attrs)
+    static_df.loc[is_a_time, "value"] = pd.to_datetime(static_df.loc[is_a_time, "value"], errors="coerce")
+    static_df.loc[is_a_time, ["value", "time"]] = static_df.loc[is_a_time, ["time", "value"]].values
+    
+    return static_df.drop_duplicates()
+    
 
-
-def add_longitudinal_features_to_patient_records(
-    patient_records: dict[int, pd.DataFrame],
+def extract_longitudinal_features(
     data_df: pd.DataFrame,
     data_key: str,
 ) -> None:
-    """ Add longitudinal features to patient records (...)
+    """ Add longitudinal features to patient records
     """
-    # Extract longitudinal features data
+    # Initial setup
     longitudinal_dict: dict = cfg.LONGITUDINAL_FEAT_INFO_DICTS[data_key]
-    for attr_field in longitudinal_dict.keys():
+    longitudinal_df = pd.DataFrame()
+    
+    # Extract longitudinal features data
+    for attr_field in tqdm(longitudinal_dict.keys(), desc=f" - Extracting longitudinal features"):
         relevant_columns = f"^(patid|currentformid|{attr_field}_|{attr_field}date_)"  # |{attr_field}oth_)"
-        longitudinal_df = data_df.filter(regex=relevant_columns)
+        attr_df = data_df.filter(regex=relevant_columns)
         
         # Melt all events to a long format  # TODO: CHECK IF THERE IS A BETTER WAY THAN USING "currentformid" to identify rows
-        longitudinal_df = pd.wide_to_long(
-            df=longitudinal_df,
+        attr_df = pd.wide_to_long(
+            df=attr_df,
             stubnames=[attr_field, f"{attr_field}date"],
             i="currentformid",
             j="seq_id",
@@ -107,19 +118,37 @@ def add_longitudinal_features_to_patient_records(
             suffix=r"\d+",
         ).reset_index().drop(columns=["seq_id", "currentformid"])
         
-        # Populate patient records with longitudinal features
-        longitudinal_df = longitudinal_df.rename(columns={attr_field: "value", f"{attr_field}date": "time"})
-        longitudinal_df = longitudinal_df.dropna(subset=["value"])
-        longitudinal_df["entity"] = data_key
-        longitudinal_df["attribute"] = attr_field
-        for pat_id, group in longitudinal_df.groupby("patid"):
-            patient_records[pat_id] = pd.concat([patient_records[pat_id], group], ignore_index=True)
-
+        # Rename columns to the EAVT format
+        attr_df = attr_df.rename(columns={attr_field: "value", f"{attr_field}date": "time"})
+        attr_df = attr_df.dropna(subset=["value"])
+        attr_df["entity"] = data_key
+        attr_df["attribute"] = attr_field
+        
+        # Fill missing dates with a default date (to distinguish from static features)
+        attr_df["time"] = attr_df["time"].fillna(pd.Timestamp("2000-01-01 00:00:00"))
+        
+        # Add identified features to the longitudinal dataframe
+        longitudinal_df = pd.concat([longitudinal_df, attr_df], ignore_index=True)
+        
+    return longitudinal_df.drop_duplicates()
+        
+        
+def add_features_to_patient_records(
+    patient_records: dict[int, pd.DataFrame],
+    features_df: pd.DataFrame,
+) -> None:
+    """ Add features to patient records
+    """
+    assert "patid" in features_df.columns, "Ensure patid column is part of features_df"
+    for pat_id, group in tqdm(features_df.groupby("patid"), desc="Populating patient records"):
+        group = group.drop(columns=["patid"])  # csv files are already named after patid
+        patient_records[pat_id] = pd.concat([patient_records[pat_id], group], ignore_index=True)
+        
         
 def get_pat_ids_with_consent(data_dict: dict[str, pd.DataFrame]) -> pd.Series:
     """ Identify patients who gave their consent for retrospective analysis
     """
-    consent_df = data_dict["#1_CONSENT"]
+    consent_df = data_dict["consent"]
     consent_given = consent_df["consstatus"] == "Consent given"
     pat_ids_with_consent = consent_df.loc[consent_given, "patid"]
     
@@ -133,9 +162,10 @@ def base_filtering(
 ) -> pd.DataFrame:
     """ Base filtering that is common to all dataframes
     """
-    df = df[~df["_STAT_"].isin(stat_rows)]  # unnecessary stat rows below patient info
-    df = df.dropna(subset=["_STAT_"])  # remove remaining blank rows around stat rows
-    df = df.drop(columns=["_STAT_"])
+    if "__STAT__" in df.columns:
+        df = df[~df["_STAT_"].isin(stat_rows)]  # unnecessary stat rows below patient info
+        df = df.dropna(subset=["_STAT_"])  # remove remaining blank rows around stat rows
+        df = df.drop(columns=["_STAT_"])
     df = df[df["patid"].isin(pat_ids_with_consent)]  # keep only patients with consent
     
     return df
--- process_data_utils.py
+++ process_data_utils.py
@@ -4,51 +4,105 @@ import pandas as pd
 class DataConfig:
     """ Configuration for the data processing pipeline
     """
-    # Paths
+    # General
+    DEBUG = False
     DATA_DIR = "data"
-    RAW_DATA_NAME = "SAS_Data_Dictionary_FUP226_raw-01Jan2023_v1.xlsx"
+    RAW_DATA_NAME = "Datacut_FUP226_raw-01Jan2023_v1.xlsx"  # "SAS_Data_Dictionary_FUP226_raw-01Jan2023_v1.xlsx"
     RAW_DATA_PATH = os.path.join(DATA_DIR, RAW_DATA_NAME)
+    RAW_DATA_PATH_PICKLE = RAW_DATA_PATH.replace(".xlsx", ".pkl")
     SAVE_DIR = os.path.join(DATA_DIR, "processed")
     
     # Features and rows taken/ignored from raw data
     STAT_ROW_NAMES = ["MIN", "P1", "P25", "MEDIAN", "P75", "P99", "MAX", "MEAN", "MODE"]
-    TAKEN_DF_NAMES = ["#2_KIDNEY_BL", "#3_KIDNEY_FUP", "#4_PAT_BL", "#5_PAT_PSQ"]
+    TAKEN_DF_NAMES = ["kidney_bl", "kidney_fup", "pat_bl", "pat_psq"]
     
+    # Useful to remove invalid date (NOT USED YET)
+    NAN_VALUES = [
+        "NaN", "Global consent refused", "Not applicable", "Unknown", ...,
+        -777.0, -888.0, -999.0, ...,
+        "9999-01-01", "2000-01-01", ...,
+    ]
+
     # Information to preprocess static patient and transplant features
-    STATIC_FEAT_INFO_DICTS = {        
-        # Kidney-transplant baseline infromation sheet
-        "#2_KIDNEY_BL": {
-            "organtype": str,  # should always be kidney?
+    STATIC_FEAT_INFO_DICTS = {
+
+        # Kidney-transplant baseline infromation sheet /!\ specific to organid /!\
+        "kidney_bl": {
             "tpxdate": pd.Timestamp,  # transplantation date
+            "hospstart": pd.Timestamp,  # hospital admission date
+            "hospend": pd.Timestamp,  # hospital discharge date
+            "dialysisdate": pd.Timestamp,  # initial dialysis type <- maybe combine to tpxdate to derive time since last dialysis?
+            "dialysistype": str,  # HD 2334, NaN 576, PD 480, Global consent refused 3 - initial dialysis type
+            "warmisch": float,  # warm ischemia time (minutes)
+            "coldischmin": float,  # cold ischemia time (minutes) - time between aorta closed at donor till re-opened at reciptient
+            "coldischmin2": float,  # cold ischemia time for second kidney (minutes), not sure how this works...
+            "tpxtype": str,  # Right 1545, NaN 1073, Left, 702, Double 53, Unknown 15, Global consent refused 5
+            "ec": str,  # No 1715, Yes 1004, Unknown 498, NaN 173, Global consent refused 3 - blood transfusion event
+            "ss": str,  # Not applicable 1448, Yes 794, NaN 718, No 401, Unknown 29 Global consent refused 3 - pregnancy event
+            "hbsag": str,  # Negative 3331, Positive 53, Unknown 3, Global consent refused 3, NaN 2, Not applicable 1 - hepatitis-B surface antigen
+            "antihbs": str,  # Positive 1970, Negative 1254, NaN 126, Unknown 40, Global consent refused 3  - antibody for hepatitis-B surface antigen
+            "antihbc": str,  # Negative 2957, Positive 347, NaN 57, Unknown 29, Global consent refused 3 - antibody for hepatitis-B core antigen
+            "antihcv": str,  # Negative 3261, Positive 123, NaN 3, Global consent refused 3, Unknown 2, Not applicable 1 - antibody for hepatitis-C virus
+            "anticmv": str,
+            "antiebv": str,
+            "antihiv": str,
+            "antitoxo": str,
+            "antivzv": str,
+            "antihsv": str,
+            "tpha": str,  # Negative 2729, NaN 362, Not applicable 222, Positive 46, Unknown 31, Global consent refused 3
+            "donage": float,  # donor age (years) -> or donbirthdate?
+            "donsex": str,  # Female 1698, Male 1690, Global consent refused 3, NaN 2 - donor sex
+            "doncod": str,  # CHE 951, Not applicable 818, ANX 555, CTR 424, NaN 420, CDI 138, Other 47, SUI 32, CTU 7, Unknown 1 - donor cause of death
+            "donorbg": str,  # A 1491, 0 1488, B 303, AB 111 - donor blood group
+            "donorpool": str,  # NaN 2512, No 575, Yes 306 - donor belongs to extended pool (true = probably less compatible)
+            "donhbsag": str,  # same tests and values as for receiver
+            "donantihbs": str,  # same tests and values as for receiver
+            "donantihbc": str,  # same tests and values as for receiver
+            "donantihcv": str,  # same tests and values as for receiver
+            "donanticmv": str,  # same tests and values as for receiver
+            "donantiebv": str,  # same tests and values as for receiver
+            "donantihiv": str,  # same tests and values as for receiver
+            "donantitoxo": str,  # same tests and values as for receiver
+            "donantivzv": str,  # same tests and values as for receiver
+            "donantihsv": str,  # same tests and values as for receiver
+            "hlaamismatch": float,  # number of HLA-A mismatches
+            "hlabmismatch": float,  # number of HLA-B mismatches
+            "hladrmismatch": float,  # number of HLA-DR mismatches
+            "sumhlamismatch": float,  # sum of HLA mismatches (provides good bias)
+            "witprim": float, # asystolic ischemia time (min), cardiac arrest till preservation solution perfusion
+            "organtype_counter": int,  # "how-many-th" transplanted organ is this one for the patient
+            "resecstcs_organtype": str,  # First 3264, Re 105, Sec 24 - re would be same organ "place" but new transplantation event
+
         },
         
         # Kidney-transplant follow-up information sheet
-        "#3_KIDNEY_FUP": {
+        "kidney_fup": {
             "tpxdate": pd.Timestamp,  # transplantation date
         },
         
         # Patient baseline information sheet
-        "#4_PAT_BL": {
+        "pat_bl": {
             "centreid": str,
             "bmi": float,
             "birthday": pd.Timestamp,
         },
         
         # Patient socio-demographic questionnaire sheet
-        "#5_PAT_PSQ": {
+        "pat_psq": {
             "ethnicity": str,
         },
     }
     
     # Information to preprocess longitudinal patient and transplant features
-    # TODO: ALWAYS CHECK FOR KEY + "oth" and KEY + "date"
-    # TODO MAYBE: KEY + "clinical" for associated clinical note
     LONGITUDINAL_FEAT_INFO_DICTS = {
         # Kidney-transplant baseline infromation sheet
-        "#2_KIDNEY_BL": {},
+        "kidney_bl": {
+            "etio": str,  # etiology of kidney disease
+            "immu": str,  # immuno-test -> /!\ also (immunum, immures, immumeth)
+        },
         
         # Kidney-transplant follow-up information sheet
-        "#3_KIDNEY_FUP": {  
+        "kidney_fup": {  
             "glo": str,  # reason of graft loss
             "rj": str,  # graft rejection, type, and grading
             "alldis": str,  # allograft disease
@@ -59,12 +113,12 @@ class DataConfig:
         },
         
         # Patient baseline information sheet
-        "#4_PAT_BL": {
+        "pat_bl": {
             "mek": str,
         },
         
         # Patient socio-demographic questionnaire sheet
-        "#5_PAT_PSQ": {},
+        "pat_psq": {},
     }
     
     # Data types for the final patient records
